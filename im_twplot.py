78510a0683983f3169fe8c50630c1e13590d9a13
NT <86037419+inverter3p@users.noreply.github.com>
2021-06-21 16:07:29 +0700

    Update im_twplot.py

--- im_twplot.py
+++ im_twplot.py
@@ -1,3 +1,17 @@
+from PIL import Image
 import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+# from scipy.integrate import cumtrapz
 
-st.write()
+st.set_page_config(
+    page_title="Induction motor Torque-speed curve",
+    page_icon="🎮",
+    initial_sidebar_state="expanded",
+)
+
+st.write(""" # Torqe-Speed Curve of Induction Motor """)
+st.info('Simplified Equivalent Circuit')
+im_eq = Image.open('simple_im.png')
+st.image(im_eq,use_column_width = True)
